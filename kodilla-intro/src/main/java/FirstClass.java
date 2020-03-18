17e8e9ea252f474c35b76142962aea073cff130e
Michal Jurewicz <jurewicz.michal@gmail.com>
2020-03-18 14:47:57 +0100

    druga wersja

--- kodilla-intro/src/main/java/FirstClass.java
+++ kodilla-intro/src/main/java/FirstClass.java
@@ -1,23 +1,32 @@
 public class FirstClass {
     public static void main(String[] args) {
 
-        Notebook notebook = new Notebook(600, 800);
-        System.out.println(notebook.weight + " " + notebook.price);
-        notebook.checkPrice();
-        notebook.checkWeight();
+        Notebook lightNotebook = new Notebook(5, 1000);
+        System.out.println(lightNotebook.weight + " " + lightNotebook.price + " " + lightNotebook.price);
+        lightNotebook.checkPrice();
+        lightNotebook.checkWeight();
 
-        Notebook heavyNotebook = new Notebook(2000, 1700);
-        System.out.println(heavyNotebook.weight + " " + heavyNotebook.price);
-        heavyNotebook.checkPrice();
-        notebook.checkWeight();
+        Notebook Notebook = new Notebook(10, 300);
+        System.out.println(Notebook.weight + " " + Notebook.price);
+        Notebook.checkPrice();
+        Notebook.checkWeight();
 
-        Notebook oldNotebook = new Notebook(1600, 300);
+        Notebook oldNotebook = new Notebook(15, 3000);
         System.out.println(oldNotebook.weight + " " + oldNotebook.price);
         oldNotebook.checkPrice();
-        notebook.checkWeight();
+        oldNotebook.checkWeight();
 
-                    }
+        Notebook oldYear = new Notebook(1986);
+        System.out.println(oldYear.year);
+        oldYear.checkYear();
 
+        Notebook newYear = new Notebook(2003);
+        System.out.println(newYear.year);
+        newYear.checkYear();
 
+        Notebook modernYear = new Notebook(2019);
+        System.out.println(modernYear.year);
+        modernYear.checkYear();
+    }
 
 }
--- kodilla-intro/src/main/java/Notebook.java
+++ kodilla-intro/src/main/java/Notebook.java
@@ -2,6 +2,7 @@ public class Notebook {
 
     int weight;
     int price;
+    int year;
 
     public Notebook(int weight, int price) {
 
@@ -15,18 +16,34 @@ public class Notebook {
             System.out.println("This notebook is very cheap.");
         } else if (this.price > 600 && this.price < 1000) {
             System.out.println("The price is good.");
+
         } else {
             System.out.println("This notebook is expensive.");
         }
     }
-        public void checkWeight() {
-            if(this.weight < 400) {
-                System.out.println("This notebook is light.");
-            } else if(this.weight > 400 && this.price < 1200) {
-                System.out.println("This notebook is normal.");
-            } else {
-                System.out.println("This notebook is heavy.");
-            }
+
+    public void checkWeight() {
+        if (this.weight < 400 && this.price < 800) {
+            System.out.println("This notebook is light.");
+        } else if (this.weight > 400 && this.weight < 1200) {
+            System.out.println("This notebook is not very heavy.");
+        } else {
+            System.out.println("This notebook is heavy.");
+        }
+        //wiem że to nie do końca jak powinno być ale rozumiem na czym polega
+
+    }
+    public Notebook(int year) {
+        this.year = year;
+    }
+    public void checkYear() {
+        if (this.year < 1995) {
+            System.out.println("This car is old.");
+        } else if (this.year > 1995 && this.year < 2005) {
+            System.out.println("This car is fairly new.");
+        } else {
+            System.out.println("This car is modern.");
         }
     }
 
+}
